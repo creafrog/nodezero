d2361b9fd45b7099a84077302599a33e9c9f9d54
nodiscc <nodiscc@gmail.com>
2014-04-21 22:27:10 +0200

    scripts: check for ${NZ_CONF_PATH}/conf.d/*.conf before trying to source them

--- tree/usr/share/nodezero/nodezero-admin.sh
+++ tree/usr/share/nodezero/nodezero-admin.sh
@@ -5,7 +5,9 @@ set -e errexit
 
 export NZ_CONF_PATH="/etc/nodezero" #sets main config file path
 source "${NZ_CONF_PATH}/nodezero.conf"
-source "${NZ_CONF_PATH}/conf.d/*.conf"
+if [ -f "${NZ_CONF_PATH}/conf.d/*.conf" ]
+	then source "${NZ_CONF_PATH}/conf.d/*.conf"
+fi
 
 source "${NZ_PATH}/functions.sh", #NZ_PATH is specified in nodezero.conf
 
