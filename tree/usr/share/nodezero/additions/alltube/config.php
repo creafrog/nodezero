2ed4eb3bf3e4d4aa85d9518b979aab2605b9110d
nodiscc <nodiscc@gmail.com>
2014-07-05 16:23:07 +0200

    add default config for alltube (remove --no-warnings and point youtube-dl path to /usr/bin/youtube-dl)

---
+++ tree/usr/share/nodezero/additions/alltube/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * PHP web interface for youtube-dl (http://rg3.github.com/youtube-dl/)
+ * Config file
+ *
+ * PHP Version 5.3.10
+ *
+ * @category Youtube-dl
+ * @package  Youtubedl
+ * @author   Pierre Rudloff <rudloff@strasweb.fr>
+ * @license  GNU General Public License http://www.gnu.org/licenses/gpl.html
+ * @link     http://rudloff.pro
+ * */
+define('YOUTUBE_DL', '/usr/bin/youtube-dl');
+define('PYTHON', '/usr/bin/python');
+define('PARAMS', '--no-playlist');
+?>
