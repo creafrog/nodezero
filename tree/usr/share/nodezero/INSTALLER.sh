20c298f6d81ad53c80ba0ec8164c36c623a62604
nodiscc <nodiscc@gmail.com>
2014-11-10 02:06:07 +0100

    webapps: install now has an install function. comment everything else.

--- tree/usr/share/nodezero/INSTALLER.sh
+++ tree/usr/share/nodezero/INSTALLER.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
 set -o errexit
-###TEMPORARY VARS
-export NZ_PATH="."
-export APACHE_DOCUMENTROOT="/var/www"
-export NZ_USER="cornizardt"
-export APACHE_GROUP="www-data"
 set -o nounset
 
 
 
-_NzListWebapps() { #Web applications installation/uninstallation menu
-WaManifests=$(find $NZ_PATH/webapps/ -type f -name "*.manifest")
-for Wa in $WaManifests
-do
-	source "$Wa"
-	if [ -d /var/www/$AppWwwDir ]; then AppStatus="INSTALLED"; else AppStatus="Not installed"; fi
-	echo " * $(basename -s .manifest $Wa) - $AppDescription - $AppStatus"
-done
+# _NzListWebapps() { #Web applications installation/uninstallation menu
+# WaManifests=$(find $NZ_PATH/webapps/ -type f -name "*.manifest")
+# for Wa in $WaManifests
+# do
+# 	source "$Wa"
+# 	if [ -d /var/www/$AppWwwDir ]; then AppStatus="INSTALLED"; else AppStatus="Not installed"; fi
+# 	echo " * $(basename -s .manifest $Wa) - $AppDescription - $AppStatus"
+# done
 
 
-SelectedApp=""
-read -p "Type the name of the app to install/uninstall (e.g. Roundcube, WordPress...): " UserSelection
-if [ -f $NZ_PATH/webapps/$UserSelection.manifest ]
-	then source $NZ_PATH/webapps/$UserSelection.manifest
-	if [ -d /var/www/$AppWwwDir ]
-		then _NzUninstallWebapp $NZ_PATH/webapps/$UserSelection.manifest
-		else _NzInstallWebapp $NZ_PATH/webapps/$UserSelection.manifest
-	fi
-fi
+# SelectedApp=""
+# read -p "Type the name of the app to install/uninstall (e.g. Roundcube, WordPress...): " UserSelection
+# if [ -f $NZ_PATH/webapps/$UserSelection.manifest ]
+# 	then source $NZ_PATH/webapps/$UserSelection.manifest
+# 	if [ -d /var/www/$AppWwwDir ]
+# 		then _NzUninstallWebapp $NZ_PATH/webapps/$UserSelection.manifest
+# 		else _NzInstallWebapp $NZ_PATH/webapps/$UserSelection.manifest
+# 	fi
+# fi
 
+# }
+
+_NzInstallWebapp() { #Install a web application
+	AppToInstall="$1"
+	"$NZ_PATH/webapps/$AppToInstall.sh" install
+
+	_NzRegenHomepage
+	#_NzFixPermissions TODO re-enable when it's verified
 }
 
-_NzInstallWebapp() {
-#if AppUrl="none", skip git clone and go to PostInstallScript
-}
-
-
-
-_NzListWebapps
