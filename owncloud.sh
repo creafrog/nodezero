4ecd8a6ee49d1c97dcae8b142e586bcf1a0f8811
nodiscc <nodiscc@gmail.com>
2015-02-04 13:27:23 +0100

    add owncloud urls to ini file, work on owncloud install script

--- owncloud.sh
+++ owncloud.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
-install() {
-    sudo aptitude install libapache2-mod-php5 php5-gd php5-sqlite  php5-mysql php5-curl  php5-intl php5-mcrypt php5-gmp php-apc  php5-imagick #TODO create a metapackage for this
-    wget https://download.owncloud.org/community/owncloud-7.0.4.tar.bz2
-    tar -xvf owncloud-7.0.4.tar.bz2
+workdir="$PWD"
+
+_main() {
+source read_ini.sh
+read_ini nz-webapps.ini
+
+case "$1" in
+    "install") _install ;;
+    *) echo "Unsupported operation";;
+esac
+}
+
+
+_install() {
+    #sudo aptitude install libapache2-mod-php5 php5-gd php5-sqlite  php5-mysql php5-curl  php5-intl php5-mcrypt php5-gmp php-apc  php5-imagick #TODO create a metapackage for this
+    cd "$workdir"
+    #wget "$INI__owncloud_url" -O owncloud-latest.tar.bz2
+    #tar -xvf owncloud-latest.tar.bz2
     #TODO check integrity via md5
     #TODO check authenticity via GPG
-    mv owncloud "$APACHE_DOCUMENTROOT/"
-    #open hostname/owncloud/install
+    if [ -d "$APACHE_DOCUMENTROOT/$INI__owncloud_dir" ]
+        then echo 'owncloud is already installed! please uninstall it before continuing.'; return 1
+    fi
     mkdir "$APACHE_DOCUMENTROOT/owncloud/data/"
-    sudo chown -R www-data:www-data "$APACHE_DOCUMENTROOT/owncloud/"{config/,data/,apps/}
+    #prompt to open hostname/owncloud/install
+    #sudo chown -R www-data:www-data "$APACHE_DOCUMENTROOT/owncloud/"{config/,data/,apps/}
     #TODO install apps
 }
+
+
+_installapps() {
+echo "not implemented"
+}
+
+########################
+
+_main "$1"
